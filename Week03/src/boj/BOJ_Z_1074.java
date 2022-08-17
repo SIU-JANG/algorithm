22a058574e743f69fb9801ca4e770892bcbdb92a
SIU-JANG <suj950203@gmail.com>
2022-08-17 10:38:40 +0900

    divide & conquer

---
+++ Week03/src/boj/BOJ_Z_1074.java
@@ -0,0 +1,17 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ_Z_1074 {
+	
+	static int N, r, c;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		r = sc.nextInt();
+		c = sc.nextInt();
+		
+		
+	}
+}
